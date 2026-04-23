7bdffd084b87446a4a1c9fd2e0eba75d42eafaf3
Jeffrey D. Michler <jdmichler@arizona.edu>
2026-04-23 13:38:25 -0700

    clean up repo

--- fix_publication_cards2.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open('publications.html', 'r') as f:
-    html = f.read()
-
-# I want to find the exact cards from aidelab. Let's see if we can just wrap the ones we can determine the exact URL for
